9b0bd2b7184b5af4ae70fe749b8cffb3986201fe
jsanchco <jsanchco@gmail.com>
2019-03-01 19:53:33 +0100

    jsanchez-WORK. Clean code

--- Solution/CA.WEB/Client/src/app/layout/dashboard/dashboard.component.ts
+++ Solution/CA.WEB/Client/src/app/layout/dashboard/dashboard.component.ts
@@ -4,23 +4,6 @@ import { MatTableDataSource } from '@angular/material';
 import { User } from '../../shared/models/user.model';
 import { UserService } from '../../shared/services/user.service';
 
-export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' }
-];
-
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
