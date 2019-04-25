da8ac4270ac7898ca0835c1ed1e50c2a94fed914
jsanchco-WORK <jsanchco@gmail.com>
2019-04-25 17:28:50 +0200

    jsanchez-HOME. Add components car

---
+++ Solution/CA.WEBSofu/Client/src/app/pages/car/car-page/car-customization/car-customization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-customization',
+  templateUrl: './car-customization.component.html',
+  styleUrls: ['./car-customization.component.scss']
+})
+export class CarCustomizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Solution/CA.WEBSofu/Client/src/app/pages/car/car.module.ts
+++ Solution/CA.WEBSofu/Client/src/app/pages/car/car.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { CarRoutingModule } from './car-routing.module';
 import { CarPageComponent } from './car-page/car-page.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarCustomizationComponent } from './car-page/car-customization/car-customization.component';
+import { CarOdometerComponent } from './car-page/car-odometer/car-odometer.component';
+
 @NgModule({
-  declarations: [CarPageComponent],
-  imports: [CommonModule, CarRoutingModule]
+  declarations: [CarPageComponent, CarCustomizationComponent, CarOdometerComponent],
+  imports: [
+    CommonModule, 
+    CarRoutingModule,
+    NgbModule
+  ]
 })
 
 export class CarModule { }
